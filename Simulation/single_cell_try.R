7a4c84e13151290a9c1ba4ca1d24b3e005cd3b61
Kexuan <stellaxuan@pku.edu.cn>
2019-07-22 15:51:18 -0400

    modify the simulation code. more revision needed.Error: cannot allocate vector of size 457.8 Mb

--- Simulation/single_cell_try.R
+++ Simulation/single_cell_try.R
@@ -19,7 +19,7 @@ gene_length <- as.numeric(as.vector(data_all[,2001]))
 
 tech_para <- list(iternum = 100, error = 1e-5, mhnum = 300, jump_rate = 0.15,
                   jump = 0.03, burnin = 50)
-source('fitDABB_function.R')
+source('/home/kl764/project/singlecell/git/scRNA-dropouts/fitDABB_function.R')
 #fit the model:
 #Y is the G*C read count matrix, total.count is a vector of each cell's total read counts,
 #batch.info is a vector, the index of the batch each cell belongs to, bio.group is a 
@@ -109,26 +109,26 @@ results
 
 
 #Visualization of the unfitted data.
-G_num <- length(Y[,1])
-C_num <- length(Y[1,])
-sample_num <- length(results$bsample[1,])
-visual <- DABB_visualize(Y, total.count, gene_length, matrix(1, G_num, C_num),
-                         b_sample_mat = matrix(0, C_num, sample_num), method = 'PCA', k = 10)
+#G_num <- length(Y[,1])
+#C_num <- length(Y[1,])
+#sample_num <- length(results$bsample[1,])
+#visual <- DABB_visualize(Y, total.count, gene_length, matrix(1, G_num, C_num),
+                        # b_sample_mat = matrix(0, C_num, sample_num), method = 'PCA', k = 10)
 ##visual <- visual$points
-pc1 <- visual[,1]
-pc2 <- visual[,2]
-pc1.16.88 <- pc1[1:27]
-pc1.16.193 <- pc1[28:35]
-pc1.8.88 <- pc1[36:45]
-pc1.8.193 <- pc1[46:54]
-pc2.16.88 <- pc2[1:27]
-pc2.16.193 <- pc2[28:35]
-pc2.8.88 <- pc2[36:45]
-pc2.8.193 <- pc2[46:54]
+#pc1 <- visual[,1]
+#pc2 <- visual[,2]
+#pc1.16.88 <- pc1[1:27]
+#pc1.16.193 <- pc1[28:35]
+#pc1.8.88 <- pc1[36:45]
+#pc1.8.193 <- pc1[46:54]
+#pc2.16.88 <- pc2[1:27]
+#pc2.16.193 <- pc2[28:35]
+#pc2.8.88 <- pc2[36:45]
+#pc2.8.193 <- pc2[46:54]
 
-p <- ggplot()
-p <- p + geom_point(data=data.frame(pc1.16.88, pc2.16.88), aes(x=pc1.16.88, y=pc2.16.88, color="16cell", shape='run0088'), size=3)
-p <- p + geom_point(data=data.frame(pc1.16.193, pc2.16.193), aes(x=pc1.16.193, y=pc2.16.193, color="16cell",shape='run00193'), size=3)
-p <- p + geom_point(data=data.frame(pc1.8.88, pc2.8.88), aes(x=pc1.8.88, y=pc2.8.88, color="8cell",shape='run0088'), size=3)
-p <- p + geom_point(data=data.frame(pc1.8.193, pc2.8.193), aes(x=pc1.8.193, y=pc2.8.193, color="8cell",shape='run00193'), size=3)
-p <- p + ggtitle('PC1 and PC2 for 16cell and 8cell in two batches')
+#p <- ggplot()
+#p <- p + geom_point(data=data.frame(pc1.16.88, pc2.16.88), aes(x=pc1.16.88, y=pc2.16.88, color="16cell", shape='run0088'), size=3)
+#p <- p + geom_point(data=data.frame(pc1.16.193, pc2.16.193), aes(x=pc1.16.193, y=pc2.16.193, color="16cell",shape='run00193'), size=3)
+#p <- p + geom_point(data=data.frame(pc1.8.88, pc2.8.88), aes(x=pc1.8.88, y=pc2.8.88, color="8cell",shape='run0088'), size=3)
+#p <- p + geom_point(data=data.frame(pc1.8.193, pc2.8.193), aes(x=pc1.8.193, y=pc2.8.193, color="8cell",shape='run00193'), size=3)
+#p <- p + ggtitle('PC1 and PC2 for 16cell and 8cell in two batches')
