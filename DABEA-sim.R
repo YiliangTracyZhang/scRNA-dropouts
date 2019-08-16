c0d93ae89f0a335591dfd0437ce920d7e00ef830
Kexuan <stellaxuan@pku.edu.cn>
2019-08-16 16:54:27 -0400

    update

--- DABEA-sim.R
+++ DABEA-sim.R
@@ -1,3 +1,10 @@
+
+#####################
+### fit function ###
+####################
+
+## Metropolis hasting algorithm
+
 MH_bc <- function(Y, Y_zero, Cell_N, Gene_N, bc, Lambda, Pi, Theta, Nu, Sigma, step, burn, K){
   # Cell_N <- ncol(Y)
   # Gene_N <- nrow(Y)
@@ -56,6 +63,8 @@ MH_bc <- function(Y, Y_zero, Cell_N, Gene_N, bc, Lambda, Pi, Theta, Nu, Sigma, s
   return(list(bc_sample=bc_sample, ita=ita, Z=Z, Zexpbc=Zexpbc))
 }
 
+## main function 
+
 fitDABEA <- function(Y, tot_read, bat_ind, bio_ind, gene_len, step=0.1, burn=50, burn_start=500, K=500, max_iter=200, stop=0.01){
  # gene_len <- gene_len/mean(gene_len)
  # tot_read <- tot_read/mean(tot_read)
@@ -68,10 +77,10 @@ fitDABEA <- function(Y, tot_read, bat_ind, bio_ind, gene_len, step=0.1, burn=50,
   #start point
   Gamma <- -0.5
   Gamma1 <- 1
-  Alpha <- 0
-  Alpha1 <- 0.1
-  Beta <- 0
-  Beta1 <- 0.1
+  Alpha <- 0.1
+  Alpha1 <- 0
+  Beta <- 0.1
+  Beta1 <- 0
   Theta <- 1
   Theta1 <- 0
   Nu <- rep(0, Cell_N)
@@ -80,14 +89,13 @@ fitDABEA <- function(Y, tot_read, bat_ind, bio_ind, gene_len, step=0.1, burn=50,
   Sigma1 <- rep(1, Cell_N)
   N_group <- length(unique(bio_ind))
   # for real data
-  # Mu <- matrix(apply(Y, 1, FUN=function(x)aggregate(x, by=list(bio_ind), mean)[,2]), ncol = N_group) + 0.01
+  # Mu <- matrix(apply(Y, 1, FUN=function(x)aggregate(x, by=list(bio_ind), mean)[,2]), ncol = N_group) + 0.01 ##
   # for simulated data
   Mu <- matrix(0,nrow=Gene_N,ncol=N_group)
   for(i in 1:N_group){
           Mu[,i]<-matrix(Y[,bio_ind==unique(bio_ind)[i]],nrow = Gene_N) %*% (1/tot_read[bio_ind==unique(bio_ind)[i]] )* (1/gene_len)
           Mu[,i] <- Mu[,i] / (length(bio_ind[bio_ind == unique(bio_ind)[i]]))
   }
-  Mu <- Mu/ (10^3) # cell num = 1000
   Mu1 <- matrix(0, nrow=Gene_N, ncol=N_group)
   bc <- rnorm(Cell_N, Nu, Sigma)
 
@@ -107,9 +115,17 @@ fitDABEA <- function(Y, tot_read, bat_ind, bio_ind, gene_len, step=0.1, burn=50,
   ita <- MH$ita
   ita <- ita/K
   Z <- MH$Z
-  # Z <- Z/K
+  Z <- Z/K
   Zexpbc <- MH$Zexpbc
-  # Zexpbc <- Zexpbc/K
+  Zexpbc <- Zexpbc/K
+  
+       #####################
+  # observe the traceplot
+  trace <- MH$bc_sample
+  trace_1 <- trace[1,]
+  p <- plot(x=1:length(trace_1), y=trace_1, type = 'n', main = paste0('trace plot for 1 iteration'), ylab = 'value', xlab = 'sampling times') + lines(x = 1:length(trace_1), y = trace_1)  
+  print(p)
+        ######################
   
   #M step
   Gamma1 <- Gamma
@@ -122,8 +138,8 @@ fitDABEA <- function(Y, tot_read, bat_ind, bio_ind, gene_len, step=0.1, burn=50,
   
   Mu <- (Y*Z)%*%t(Group_Matrix)/sweep((sweep(Zexpbc, 2, tot_read, FUN='*')%*%t(Group_Matrix)), 1, gene_len, FUN='*')
   
-  Lambda <- sweep(sweep(Mu, 1, gene_len, FUN='*')%*%Group_Matrix, 2, tot_read, FUN = '*') ####
-  Pi <- sweep(sweep(matrix(Gamma, nrow=Gene_N, ncol=Cell_N), 2, Alpha*log(tot_read), FUN='+'), 1, Beta*log(gene_len), FUN='+') ###
+  Lambda <- sweep(sweep(Mu, 1, gene_len, FUN='*')%*%Group_Matrix, 2, tot_read, FUN = '*') #### 08/16 newly added
+  Pi <- sweep(sweep(matrix(Gamma, nrow=Gene_N, ncol=Cell_N), 2, Alpha*log(tot_read), FUN='+'), 1, Beta*log(gene_len), FUN='+') ### 08/16 newly added
   
   bc <- rowMeans(bc_sample)
   # ita <- ita/K
@@ -202,8 +218,6 @@ fitDABEA <- function(Y, tot_read, bat_ind, bio_ind, gene_len, step=0.1, burn=50,
     # Mu = Mu * exp(meanNu)
     # Gamma = Gamma + Theta*meanNu
     
-    j <- j + 1
-    
     print(c(date(),paste0('the', j , 'iteration')))
     print(c('error', max(abs(Alpha1-Alpha), abs(Beta1-Beta), abs(Theta1-Theta), abs(Nu1-Nu), abs(Sigma1-Sigma))))
     print(c('Mu error', max(abs(Mu1-Mu))))
@@ -214,6 +228,8 @@ fitDABEA <- function(Y, tot_read, bat_ind, bio_ind, gene_len, step=0.1, burn=50,
     print(Theta)
     print(unique(Nu))
     print(unique(Sigma))
+    
+    j <- j + 1
   }
   
   print(paste0(j,' iterations in total'))
@@ -227,5 +243,14 @@ fitDABEA <- function(Y, tot_read, bat_ind, bio_ind, gene_len, step=0.1, burn=50,
   print(unique(Nu))
   print(unique(Sigma))
   
-  return(list(Mu=Mu, Alpha=Alpha, Beta=Beta, Gamma=Gamma, Theta=Theta, Nu=Nu, Sigma=Sigma, bc=bc))
+  #####dropout imputation
+  Y_impute <- Y + Lambda[Y_zero]
+  Z_matrix <- Z
+  ##### batch effect sampling (stored for quality control)
+  sampling <- bc_sample
+  
+  return(list(Mu=Mu, 
+              Alpha=Alpha, Beta=Beta, Gamma=Gamma, Theta=Theta, Nu=Nu, Sigma=Sigma, 
+              Y_impute = Y_impute, Z_matrix = Z_matrix,
+              bc=bc, sampling = sampling))
 }
